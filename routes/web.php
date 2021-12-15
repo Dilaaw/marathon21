08510f605df83efee4634f411c9323f3675ee0b3
Rahioui Walid <rahiouiwalid@gmail.com>
2021-12-15 15:43:29 +0100

    recup

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get("/profil", function () {
     return view('profil');
 })->name("profil");
 
-Route::get("/liste",[\App\Http\Controllers\ListeController::class, 'getListe'])->name('welcome');
+Route::get("/liste",[\App\Http\Controllers\ListeController::class, 'getListe'])->name('liste');
 
 Route::fallback(function () {
     return view('404');
