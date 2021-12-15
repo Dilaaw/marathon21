38541ec7409f630c1320de963b6c1ed85d236428
christopher <christopher.dutoit2002@gmail.com>
2021-12-15 18:04:45 +0100

    Début integration accueil

--- public/js/app.js
+++ public/js/app.js
@@ -2062,6 +2062,13 @@ module.exports = {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+var LastSeries = document.getElementsByClassName('last-series');
+LastSeries[0].classList.add('last-series3');
+LastSeries[1].classList.add('last-series2');
+LastSeries[2].classList.add('last-series1');
+LastSeries[3].classList.add('last-series2');
+LastSeries[4].classList.add('last-series2');
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,5 +42,6 @@
     @yield('content')
 </div>
 <!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
